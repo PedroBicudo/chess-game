445bd54a84789a0c2f1f7d211b77273e46396a18
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-02-21 16:32:23 -0300

    feat: add move piece action

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,10 +1,29 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(in);
+
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(in);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
     }
 }
