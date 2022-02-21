9468ea1cd1116845c9f3164128e45e3b241cfbf5
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-02-21 16:14:48 -0300

    feat(chess): add ChessPosition

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -16,14 +16,18 @@ public class ChessMatch {
 
     public void initialSetup() {
         // Black
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 0));
-        board.placePiece(new Rook(board, Color.BLACK), new Position(0, 7));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
 
         // White
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 0));
-        board.placePiece(new Rook(board, Color.WHITE), new Position(7, 7));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+    }
+
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
     }
 
     public ChessPiece[][] getPieces() {
