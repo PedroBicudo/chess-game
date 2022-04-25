22f8c3b38bed71cfdca5b18a678556c300a25a5e
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-04-25 12:16:16 -0300

    feat: add special move Castling

--- src/main/java/chess/pieces/King.java
+++ src/main/java/chess/pieces/King.java
@@ -2,13 +2,18 @@ package chess.pieces;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -70,6 +75,29 @@ public class King extends ChessPiece {
             mat[p.getRow()][p.getColumn()] = true;
         }
 
+        // #special-move: Castling
+        if (getMoveCount() == 0 && !chessMatch.isCheck()) {
+
+            // castling (right)
+            p.setValues(position.getRow(), position.getColumn()+3);
+            if (testRookCastling(p)) {
+                Position p1 = new Position(position.getRow(), position.getColumn()+1);
+                Position p2 = new Position(position.getRow(), position.getColumn()+2);
+                mat[position.getRow()][position.getColumn()+2] = canMove(p1, p2);
+            }
+
+            // castling (left)
+            p.setValues(position.getRow(), position.getColumn()-4);
+            if (testRookCastling(p)) {
+                Position p1 = new Position(position.getRow(), position.getColumn()-1);
+                Position p2 = new Position(position.getRow(), position.getColumn()-2);
+                Position p3 = new Position(position.getRow(), position.getColumn()-3);
+                mat[position.getRow()][position.getColumn()-2] = canMove(p1, p2, p3);
+
+            }
+
+        }
+
         return mat;
     }
 
@@ -81,6 +109,21 @@ public class King extends ChessPiece {
         ChessPiece chessPiece = (ChessPiece) getBoard().piece(target);
         return chessPiece == null ||
                 chessPiece.getColor() != getColor();
+    }
+
+    private boolean canMove(Position ...targets) {
+        for (Position p: targets) {
+            if (getBoard().piece(p) != null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean testRookCastling(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
+        return p != null && p instanceof Rook && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
 }
